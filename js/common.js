e75ade5692acd2db01281c980f6f65ce24483667
matsu <qandamatsu60@gmail.com>
2020-09-27 17:29:43 +0900

    instagram feed / pagetop ボタン　追加

--- js/common.js
+++ js/common.js
@@ -62,3 +62,24 @@ window.onload = function () {
     $('.hero_img p').addClass('hero_fadein');
   };
 };
+
+//ページトップボタン
+$(function () {
+  var topBtn = $('#page_top');
+  topBtn.hide();
+  // スクロールが100に達したらボタン表示
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 500) {
+      topBtn.fadeIn();
+    } else {
+      topBtn.fadeOut();
+    }
+  });
+  //スクロールしてトップ
+  topBtn.click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 500);
+    return false;
+  });
+});
